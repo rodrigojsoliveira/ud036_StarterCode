e57a1290efc68ebbda925d73c94ee9419fffb094
Rodrigo Jose Sarmento Oliveira <rodrigojsoliveira@gmail.com>
2018-09-30 20:13:07 -0300

    First working version of movie list generator.
    Still missing trailer links.
    There is a weird bug while displaying the movies. Some empty spaces are displayed in between movie divs.

--- movie_list_generator.py
+++ movie_list_generator.py
@@ -1,11 +1,49 @@
 import movie
 import fresh_tomatoes
+import urllib.request
+import json
 
-a_bugs_life = movie.Movie("A Bug's Life",
-                          "https://vignette.wikia.nocookie.net/pixar/images/c/ca/Bugs_life_ver5_xlg.jpg/revision/latest?cb=20110515135005",
-                          "https://www.youtube.com/watch?v=vhGlMv3UCXA")
+# 'The Movie Database' API constants. IMAGE_BASE_URL and POSTER_FILE_SIZE
+# were retrieved using this link:
+# https://developers.themoviedb.org/3/configuration/get-api-configuration
+# Since these values do not change often, a specific method to retrieve the
+# data was not implemented.
+API_KEY = "fa71f4f6a5a629f8e49e3ed1bfe31a6e"
+IMAGE_BASE_URL = "https://image.tmdb.org/t/p/"
+POSTER_FILE_SIZE = "w500"
 
-movies = [a_bugs_life]
 
+# Retrieves a list of trending movies in the past week from
+# 'The Movie Database' website using its API and returns a JSON object
+# containing the full list.
+def getTrendingMovies():
+
+    url = "https://api.themoviedb.org/3/trending/movie/week?api_key=" + API_KEY
+    http_response = urllib.request.urlopen(url,None)
+    jsonObject = json.load(http_response)
+    return jsonObject
+
+
+# Creates an instance of class Movie from a JSON object.
+def createMovie(movieObject):
+    title = movieObject['title']   
+    poster = IMAGE_BASE_URL + POSTER_FILE_SIZE + movieObject['poster_path']
+    trailer = ""
+    movieInstance = movie.Movie(title, poster, trailer)
+    return movieInstance
+
+
+# Returns a list of Movie object to be used by the
+# fresh_tomatoes.open_movies_page method.
+def createMovieList():
+    movieList = []
+    trendingMovies = getTrendingMovies()
+    for result in trendingMovies['results']:
+        newMovie = createMovie(result)
+        movieList.append(newMovie)
+    return movieList
+
+
+# Main Code    
+movies = createMovieList()
 fresh_tomatoes.open_movies_page(movies)
-                          
