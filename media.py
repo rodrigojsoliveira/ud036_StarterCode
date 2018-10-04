e56a65f404e1b90f1f5d0a6a96f3fb722369876c
Rodrigo Jose Sarmento Oliveira <rodrigojsoliveira@gmail.com>
2018-10-03 21:27:10 -0300

    Include docstrings in media.py.

--- media.py
+++ media.py
@@ -1,4 +1,19 @@
+"""
+Media Module
+
+This module contains entertainment medias. Examples of media that could
+be modelled are TV Shows, Music Concerts, Sports Broadcast, and so on.
+"""
+
+
 class Movie():
+    """
+    Class Movie
+
+    A movie object will contain basic information of a movie, like its title,
+    poster image url, and trailer url. Other properties can be created
+    depending on the information you wish to display to the end user.
+    """
 
     # Class constructor.
     def __init__(self, title, poster_image_url, trailer_youtube_url):
